4ac613dca1b6ec41629715a0480c71a42e7e2036
Accel <accelwo@dcomp.ufs.br>
2018-08-28 11:37:32 -0300

    iniciando a consulta HTTP

--- src/main/java/br/ufs/dcomp/Chat/Chat.java
+++ src/main/java/br/ufs/dcomp/Chat/Chat.java
@@ -9,6 +9,13 @@ import java.nio.file.spi.FileTypeDetector;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
 import java.util.*;
 import java.util.Scanner;
 import java.text.DateFormat;
@@ -284,6 +291,27 @@ public class Chat {
               System.out.println("Caminho atual é = " + dir);
               break;
             case "!listUsers":
+              System.out.println("listUsers: ");
+              try{
+                URL url = new URL ("http://ec2-54-200-22-72.us-west-2.compute.amazonaws.com:15672/api/exchanges/%2F/" 
+                        +corte[1]+"/bindings/source?columns=destination");
+                String encoding = Base64.getEncoder().encodeToString(("accel:@Accel27").getBytes("UTF-8"));
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoOutput(true);
+                conn.setRequestProperty  ("Authorization", "Basic " + encoding);
+                InputStream content = (InputStream)conn.getInputStream();
+                BufferedReader in = new BufferedReader(new InputStreamReader(content));
+                String linha;
+                while ((linha = in.readLine()) != null) {
+                  System.out.println(linha);
+                }
+                
+              } catch (Exception e){
+                e.printStackTrace();
+              }
+              
+              
               break;
             case "!listGroups":
               break;
